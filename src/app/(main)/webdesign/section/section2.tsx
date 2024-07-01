d04ac501466dfcbcb56b336ca59dca63b42f1729
ghost91202 <karansharma8543@gmail.com>
2024-07-02 02:44:27 +0530

    make resposive

--- src/app/(main)/webdesign/section/section2.tsx
+++ src/app/(main)/webdesign/section/section2.tsx
@@ -10,11 +10,11 @@ const section2 = (props: Props) => {
       <div className='bg-slate-950 h-auto flex flex-col items-center pt-20 justify-center'>
           <div className=' flex flex-col gap-10 items-center justify-center'>
               <span className='bg-gray-800/80 p-1 px-4 rounded-full w-fit text-white'>UX/UI</span>
-              <h1 className='sm:text-7xl font-semibold'>The Interplay of UI and UX</h1>
+              <h1 className='sm:text-7xl text-2xl  font-semibold'>The Interplay of UI and UX</h1>
               <p className=' text-center sm:w-[1000px]'>While UI focuses on the tangible elements, UX addresses the overall feel and effectiveness of the experience. Think of UI as the tools a user needs for their journey—elegant, precise, and pleasing. UX is the path itself—the ease of navigation, the anticipation of their needs, and the satisfaction of reaching their destination effortlessly. Together, they create a harmonious digital environment that not only looks exceptional but also feels right.</p>
               <div className='flex gap-5'>
-                  <Image src={UXUI} alt='Vistacraze' width={900} height={400}/>
-                  <Image src={UXUI2} alt='vistacraze' width={300}/>
+                  <Image src={UXUI} alt='Vistacraze' width={200}  className='sm:w-auto w-[200]'/>
+                  <Image src={UXUI2} alt='vistacraze' width={100}/>
               </div>
           </div>
     </div>
