9a6cddada9bd0d406c1c5a320ab3161a90753fea
ghost91202 <karansharma8543@gmail.com>
2024-07-02 17:08:02 +0530

    optimize webdesign

--- src/app/(main)/webdesign/page.tsx
+++ src/app/(main)/webdesign/page.tsx
@@ -4,6 +4,9 @@ import Stickycard2 from "@/components/cards/stickycard2";
 import NavbarDemo from "@/components/navbar/navbar";
 import Section2 from "./section/section2";
 import Section3 from "./section/section3";
+import Footer from "@/components/footer/footer";
+import Industries from "@/components/industry/industries";
+
 export default function Home() {
 
 
@@ -13,7 +16,9 @@ export default function Home() {
       <Bgblur />
       <Stickycard2 />
       <Section2 />
-      <Section3/>
+      <Section3 />
+      <Industries />
+      <Footer/>
     </div>
   );
 }
