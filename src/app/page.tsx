7b911b02fc39489110e4a6273d851e782f123eed
ghost91202 <karansharma8543@gmail.com>
2024-07-13 09:05:03 +0530

    seo optimize

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -150,7 +150,7 @@ export default function Home() {
 
           </div>
         </div>
-              <Navbar />
+              {/* <Navbar /> */}
 
         <Industries />
         <Testimonials/>
