9845cd21872b67bb9503e0991fdba10922b893f2
ghost91202 <karansharma8543@gmail.com>
2024-07-14 14:43:15 +0530

    optimize web design hero

--- src/app/(main)/webdesign/hero/hero.tsx
+++ src/app/(main)/webdesign/hero/hero.tsx
@@ -7,15 +7,15 @@ type Props = {}
 
 const hero = (props: Props) => {
   return (
-    <div className='bg-black xl:h-screen text-white h-screen  grid grid-flow-row grid-cols-1 sm:grid-cols-2 sm:p-20 items-center justify-center gap-10'>
-      <div className='flex flex-col gap-10'>
-        <h1 className='sm:text-2xl lg:text-7xl '>
+    <div className='bg-black  text-white h-screen  grid grid-flow-row grid-cols-1 sm:grid-cols-2 sm:p-20 items-center justify-center p-5 sm:gap-10'>
+      <div className='flex flex-col sm:mt-0 mt-20 gap-10'>
+        <h1 className='text-5xl lg:text-7xl '>
           Unlimited Design, One Monthly Price
         </h1>
         <p className=''>Elevate Your Business with Professional Design Service. One Affordable Monthly Fee. Your Design, Your Terms. Transform Your Business today!</p>
-        <div className='flex gap-10'>
-          <Link href="#" className='bg-violet-600 rounded-md px-5 py-3'>See plans</Link>
-          <Link href="#" className='border-2 rounded-md px-5 py-3'>How it works</Link>
+        <div className='flex sm:flex-row flex-col gap-10'>
+          <Link href="#" className='bg-violet-600 rounded-md px-5 py-3 items-center flex justify-center'>See plans</Link>
+          <Link href="#" className='border-2 rounded-md px-5 py-3 items-center flex justify-center'>How it works</Link>
         </div>
       </div>
       <div>
