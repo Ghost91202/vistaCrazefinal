cadffff0e0a785356044287809075124d87bd883
ghost91202 <karansharma8543@gmail.com>
2024-07-15 09:53:14 +0530

    added drawer

--- src/app/(main)/uiux/page.tsx
+++ src/app/(main)/uiux/page.tsx
@@ -1,10 +1,8 @@
 "use client";
 import Bgblur from "@/components/bgblur/blurbg"
 import Stickycard2 from "@/components/cards/stickycard2";
-import NavbarDemo from "@/components/navbar/navbar";
 import Section2 from "./section/section2";
 import Section3 from "./section/section3";
-import Footer from "@/components/footer/footer";
 import Industries from "@/components/industry/industries";
 import Faq from "@/components/faq/faq"
 export default function Home() {
@@ -12,14 +10,12 @@ export default function Home() {
 
   return (
     <div >
-      <NavbarDemo />
       <Bgblur />
       <Stickycard2 />
       <Section2 />
       <Section3 />
       <Industries />
       <Faq/>
-      <Footer/>
     </div>
   );
 }
